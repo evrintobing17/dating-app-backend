d67dbb3c23dd98e0c6f7c5a6f7256a24ed45b338
evrintobing17 <evrintobing@gmail.com>
2024-12-20 21:11:41 +0700

    add auth module

---
+++ internal/module/auth/pg_repository.go
@@ -0,0 +1,12 @@
+package auth
+
+import (
+	"context"
+
+	"github.com/evrintobing17/dating-app-go/internal/models"
+)
+
+type AuthRepository interface {
+	Login(ctx context.Context, email string) (*models.User, error)
+	SignUp(query string, user *models.User) error
+}
---
+++ internal/module/auth/repository/auth_repository.go
@@ -0,0 +1,39 @@
+package repository
+
+import (
+	"context"
+	"errors"
+
+	"github.com/evrintobing17/dating-app-go/internal/models"
+	"github.com/evrintobing17/dating-app-go/internal/module/auth"
+	"github.com/evrintobing17/dating-app-go/internal/repository"
+	"github.com/lib/pq"
+)
+
+type authRepo struct {
+	db *repository.Database
+}
+
+func NewAuthRepository(db *repository.Database) auth.AuthRepository {
+	return &authRepo{db: db}
+}
+
+func (a *authRepo) Login(ctx context.Context, email string) (*models.User, error) {
+	user := &models.User{}
+	query := "SELECT id, name, email, password, is_premium FROM users WHERE email=$1"
+	err := a.db.Conn.QueryRow(query, email).Scan(&user.ID, &user.Name, &user.Email, &user.Password, &user.IsPremium)
+
+	return user, err
+}
+
+func (a *authRepo) SignUp(query string, user *models.User) error {
+	_, err := a.db.Conn.Exec(query, user.Name, user.Email, user.Password, user.IsPremium)
+	if err != nil {
+		if pqErr, ok := err.(*pq.Error); ok {
+			if pqErr.Code == "23505" {
+				return errors.New("user already exists")
+			}
+		}
+	}
+	return err
+}
