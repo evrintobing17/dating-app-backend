48ba06d0dd32d40f23186375d12d69ecc06ca873
evrintobing17 <evrintobing@gmail.com>
2024-12-20 23:19:21 +0700

    fix auth middle ware

--- internal/middleware/auth_middleware.go
+++ internal/middleware/auth_middleware.go
@@ -2,12 +2,22 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
+	"github.com/evrintobing17/dating-app-go/internal/module/auth"
 	"github.com/evrintobing17/dating-app-go/internal/utils"
 	"github.com/gin-gonic/gin"
 )
 
-func AuthMiddleware() gin.HandlerFunc {
+type authMiddleware struct {
+	authService auth.AuthRepository
+}
+
+func NewAuthMiddleware(authService auth.AuthRepository) AuthMiddleware {
+	return &authMiddleware{authService: authService}
+}
+
+func (auth *authMiddleware) AuthMiddleware() gin.HandlerFunc {
 	return func(c *gin.Context) {
 		token := c.GetHeader("Authorization")
 		if token == "" {
@@ -15,8 +25,15 @@ func AuthMiddleware() gin.HandlerFunc {
 			c.Abort()
 			return
 		}
+		jwtTokenSplit := strings.Split(token, "Bearer ")
+		if jwtTokenSplit[1] == "" {
+			c.JSON(http.StatusUnauthorized, gin.H{"error": "invalid token"})
+			c.Abort()
+			return
+		}
+		jwtToken := jwtTokenSplit[1]
 
-		claims, err := utils.ParseToken(token)
+		claims, err := utils.ParseToken(jwtToken)
 		if err != nil {
 			c.JSON(http.StatusUnauthorized, gin.H{"error": "Invalid token"})
 			c.Abort()
